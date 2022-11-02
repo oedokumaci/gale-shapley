d489eea7ed5aad00b48a96cc3c98aa2f777be4aa
oedokumaci <oral.ersoy.dokumaci@gmail.com>
2022-11-02 10:49:31 +0300

    Feat: Add module docstring, base exception

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,12 +1,19 @@
-class TwoSidedMatchingError(Exception):
-    def __init__(self, genders: list[str]) -> None:
-        self.message = (
-            f"There are {len(genders)} sides to be matched. There should be exactly two"
-        )
+"""Exceptions for the project."""
+
+
+class GSException(Exception):
+    """Base exception for the project."""
+
+    pass
+
+
+class TwoSidedMatchingError(GSException):
+    def __init__(self, gender_names: list[str]) -> None:
+        self.message = f"There are {len(gender_names)} sides to be matched. There should be exactly two"
         super().__init__(self.message)
 
 
-class GenderError(Exception):
+class GenderError(GSException):
     def __init__(self, gender_names: list[str]) -> None:
         gender_names_in_quotes = [f"'{g}'" for g in gender_names]
         self.message = f'Gender should be a {" or a ".join(gender_names_in_quotes)}'
