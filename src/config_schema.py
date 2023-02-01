a17c9b2a5981723bbc2eac238676dcd898c04a04
oral.ersoy.dokumaci <oral.ersoy.dokumaci@gmail.com>
2023-02-01 16:17:56 +0300

    Style run all hooks

--- src/config_schema.py
+++ src/config_schema.py
@@ -23,9 +23,13 @@ def _parse_proposer_and_responder(config: dict) -> tuple[str, str]:
     try:
         proposer_and_responder = config["proposer_and_responder"]
     except (KeyError, TypeError):
-        raise ConfigError("proposer_and_responder is not specified properly in config.yaml, see example_config.yaml for an example")
+        raise ConfigError(
+            "proposer_and_responder is not specified properly in config.yaml, see example_config.yaml for an example"
+        )
     if not isinstance(proposer_and_responder, list):
-        raise ConfigError("proposer_and_responder should be specified as a list in config.yaml, see example_config.yaml for an example")
+        raise ConfigError(
+            "proposer_and_responder should be specified as a list in config.yaml, see example_config.yaml for an example"
+        )
     if len(proposer_and_responder) != 2:
         raise TwoSidedMatchingError(proposer_and_responder)
     proposer, responder = proposer_and_responder
@@ -38,7 +42,9 @@ def _parse_proposer_and_responder(config: dict) -> tuple[str, str]:
 
 
 if __name__ == "__main__":
-    path_to_config_yaml = os.path.join(os.path.dirname(__file__), "../config/config.yaml")
+    path_to_config_yaml = os.path.join(
+        os.path.dirname(__file__), "../config/config.yaml"
+    )
     with open(path_to_config_yaml) as config_yaml:
         config = yaml.safe_load(config_yaml)
     PROPOSER, RESPONDER = _parse_proposer_and_responder(config)
--- src/exceptions.py
+++ src/exceptions.py
@@ -11,6 +11,7 @@ class ConfigError(GSException):
 
 class TwoSidedMatchingError(ConfigError):
     """Exception class for when there are not exactly two sides to be matched."""
+
     def __init__(self, side_names: list[str]) -> None:
         self.message = f"Config input has {len(side_names)} sides to be matched. There should be exactly two"
         super().__init__(self.message)
@@ -18,6 +19,7 @@ class TwoSidedMatchingError(ConfigError):
 
 class SideNameError(ConfigError):
     """Exception class for when a side name is not in SIDE_NAMES."""
+
     def __init__(self, side: str, side_names: list[str]) -> None:
         side_names_in_quotes = [f"'{g}'" for g in side_names]
         self.message = f'{side} should be {" or ".join(side_names_in_quotes)}'
