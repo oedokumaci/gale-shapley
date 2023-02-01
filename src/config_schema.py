f1ec318e1f645b9b672b431ab06f0025942ef373
oral.ersoy.dokumaci <oral.ersoy.dokumaci@gmail.com>
2023-02-01 13:24:15 +0300

    Feat put checks into name equals main block

--- src/config_schema.py
+++ src/config_schema.py
@@ -27,23 +27,24 @@ def _parse_proposer_and_responder(
     return proposer, responder
 
 
-path_to_config_yaml = os.path.join(os.path.dirname(__file__), "../config/config.yaml")
-with open(path_to_config_yaml) as config_yaml:
-    config = yaml.safe_load(config_yaml)
+if __name__ == "__main__":
+    path_to_config_yaml = os.path.join(os.path.dirname(__file__), "../config/config.yaml")
+    with open(path_to_config_yaml) as config_yaml:
+        config = yaml.safe_load(config_yaml)
 
-SIDE_NAMES = config["SIDE_NAMES"]
-if not isinstance(SIDE_NAMES, list):
-    raise ConfigError("SIDE_NAMES should be a list")
+    SIDE_NAMES = config["SIDE_NAMES"]
+    if not isinstance(SIDE_NAMES, list):
+        raise ConfigError("SIDE_NAMES should be specified as a list, e.g. SIDE_NAMES: ['man', 'woman']")
 
-if len(SIDE_NAMES) != 2:
-    raise TwoSidedMatchingError(SIDE_NAMES)
+    if len(SIDE_NAMES) != 2:
+        raise TwoSidedMatchingError(SIDE_NAMES)
 
-try:
-    PROPOSER, RESPONDER = _parse_proposer_and_responder("PROPOSER", config, SIDE_NAMES)
-except KeyError:
     try:
-        PROPOSER, RESPONDER = _parse_proposer_and_responder(
-            "RESPONDER", config, SIDE_NAMES
-        )
+        PROPOSER, RESPONDER = _parse_proposer_and_responder("PROPOSER", config, SIDE_NAMES)
     except KeyError:
-        raise ConfigError("Either PROPOSER or RESPONDER has to be in config.yaml")
+        try:
+            PROPOSER, RESPONDER = _parse_proposer_and_responder(
+                "RESPONDER", config, SIDE_NAMES
+            )
+        except KeyError:
+            raise ConfigError("Either PROPOSER or RESPONDER has to be in config.yaml")
