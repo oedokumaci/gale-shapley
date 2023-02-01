57ea8977b7133b4ff64319bc04e128100e77d16e
oral.ersoy.dokumaci <oral.ersoy.dokumaci@gmail.com>
2023-02-01 13:19:06 +0300

    Fix error super class is specific

--- src/exceptions.py
+++ src/exceptions.py
@@ -9,14 +9,14 @@ class ConfigError(GSException):
     """Exception class for errors in the config.yaml file."""
 
 
-class TwoSidedMatchingError(GSException):
+class TwoSidedMatchingError(ConfigError):
     """Exception class for when there are not exactly two sides to be matched."""
     def __init__(self, side_names: list[str]) -> None:
         self.message = f"There are {len(side_names)} sides to be matched. There should be exactly two"
         super().__init__(self.message)
 
 
-class SideNameError(GSException):
+class SideNameError(ConfigError):
     """Exception class for when a side name is not in SIDE_NAMES."""
     def __init__(self, side: str, side_names: list[str]) -> None:
         side_names_in_quotes = [f"'{g}'" for g in side_names]
