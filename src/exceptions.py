20ca969d0dc1772a5ac317eaf9146ffa70bba1d7
oral.ersoy.dokumaci <oral.ersoy.dokumaci@gmail.com>
2023-02-01 16:21:02 +0300

    Style consistency when referring to config.yaml

--- src/exceptions.py
+++ src/exceptions.py
@@ -13,7 +13,7 @@ class TwoSidedMatchingError(ConfigError):
     """Exception class for when there are not exactly two sides to be matched."""
 
     def __init__(self, side_names: list[str]) -> None:
-        self.message = f"Config input has {len(side_names)} sides to be matched. There should be exactly two"
+        self.message = f"Config.yaml has {len(side_names)} side(s) to be matched. There should be exactly two"
         super().__init__(self.message)
 
 
