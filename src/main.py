a2171d0d915fe5d5922b10230b756528e3cb073e
oral.ersoy.dokumaci <oral.ersoy.dokumaci@gmail.com>
2023-02-03 15:04:48 +0300

    Add jupytext outputs These auto add when open as jupyter notebook

--- src/main.py
+++ src/main.py
@@ -8,6 +8,10 @@
 #       format_name: percent
 #       format_version: '1.3'
 #       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
 # ---
 
 # %%
@@ -63,5 +67,3 @@ if __name__ == "__main__":
     )
     gs_algorithm = Algorithm(proposers, responders)
     gs_algorithm.run()
-
-# %%
