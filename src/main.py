617a4dfb0a7996f748c87e504de6d8568fd3b2c6
oral.ersoy.dokumaci <oral.ersoy.dokumaci@gmail.com>
2023-02-03 14:46:48 +0300

    Feat major changes in main.py main.py is now also jupytext transformed

--- src/main.py
+++ src/main.py
@@ -1,36 +1,67 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+# ---
+
+# %%
 import random
 
-from gs_algorithm import gs_algorithm
-from person import Person
+from algorithm import Algorithm
+from config_parser import NUMBER_OF_PROPOSERS, NUMBER_OF_RESPONDERS, PROPOSER, RESPONDER
+from proposer_responder import Proposer, Responder
 
 
-def make_persons(n: int) -> None:
-    first_gender, second_gender = Person.genders[0], Person.genders[1]
-    first_gender_first_char, second_gender_first_char = (
-        first_gender[0],
-        second_gender[0],
-    )
-    if first_gender_first_char != second_gender_first_char:
-        for i in range(1, int(n / 2) + 1):
-            Person(f"{first_gender_first_char}{i}", first_gender)
-            Person(f"{second_gender_first_char}{i}", second_gender)
+# %%
+def create_objects(
+    proposer_name: str,
+    responder_name: str,
+    number_of_proposers: int,
+    number_of_responders: int,
+) -> tuple[list[Proposer], list[Responder]]:
+    if proposer_name[0] != responder_name[0]:
+        proposers = [
+            Proposer(f"{proposer_name[0]}_{i+1}", proposer_name)
+            for i in range(number_of_proposers)
+        ]
+        responders = [
+            Responder(f"{responder_name[0]}_{i+1}", responder_name)
+            for i in range(number_of_responders)
+        ]
+    else:
+        proposers = [
+            Proposer(f"{proposer_name}_{i+1}", proposer_name)
+            for i in range(number_of_proposers)
+        ]
+        responders = [
+            Responder(f"{responder_name}_{i+1}", responder_name)
+            for i in range(number_of_responders)
+        ]
+
+    for proposer in proposers:
+        select_from = [proposer] + responders
+        random.shuffle(select_from)
+        proposer.preferences = tuple(select_from)
+    for responder in responders:
+        select_from = [responder] + proposers
+        random.shuffle(select_from)
+        responder.preferences = tuple(select_from)
+
+    return proposers, responders
 
 
-def randomize_preferences(person: Person) -> None:
-    opposite_gender_persons = Person.persons[person.get_opposite_gender()]
-    person.set_preferences(
-        tuple(random.sample(opposite_gender_persons, len(opposite_gender_persons)))
-    )
-
-
-NUMBER_OF_PERSONS = 500
-
-Person.delete_persons()
-make_persons(NUMBER_OF_PERSONS)
-for v in Person.persons.values():
-    for person in v:
-        randomize_preferences(person)
-
-
+# %%
 if __name__ == "__main__":
-    gs_algorithm(Person)
+    proposers, responders = create_objects(
+        PROPOSER, RESPONDER, NUMBER_OF_PROPOSERS, NUMBER_OF_RESPONDERS
+    )
+    gs_algorithm = Algorithm(proposers, responders)
+    gs_algorithm.run()
+
+# %%
