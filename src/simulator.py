841e2b27649a7f632b52b11aff097605263e1b4b
oral.ersoy.dokumaci <oral.ersoy.dokumaci@gmail.com>
2023-02-09 23:24:48 +0300

    Feat create_objects returns This is in case to be needed like this

--- src/simulator.py
+++ src/simulator.py
@@ -8,13 +8,15 @@ from proposer_responder import Proposer, Responder
 
 
 class Simulator:
+    """Simulator class. Creates objects and runs the algorithm."""
+
     def __init__(
         self,
         proposer_name: str,
         responder_name: str,
         num_proposers: int,
         num_responders: int,
-    ):
+    ) -> None:
         self.proposer_name = proposer_name
         self.responder_name = responder_name
         self.num_proposers = num_proposers
@@ -24,7 +26,7 @@ class Simulator:
         self.number_of_simulations: int = 100
         self.results: Union[list[Algorithm], None] = None
 
-    def create_objects(self) -> None:
+    def create_objects(self) -> tuple[list[Proposer], list[Responder]]:
         """Creates the objects for the algorithm.
 
         Returns:
@@ -58,15 +60,15 @@ class Simulator:
             random.shuffle(select_from)
             responder.preferences = tuple(select_from)
 
-        self.proposers, self.responders = proposers, responders
+        proposers, responders
 
     def run(
         self, print_all_preferences: bool = True, report_matches: bool = True
     ) -> None:
-        """Runs the algorithm."""
+        """Simulates the algorithm desired number of times."""
         self.results = []
         for _ in range(self.number_of_simulations):
-            self.create_objects()
+            self.proposers, self.responders = self.create_objects()
             algorithm = Algorithm(self.proposers, self.responders)
             algorithm.run(print_all_preferences, report_matches)
             self.results.append(algorithm)
