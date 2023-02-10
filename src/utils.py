4e3eb5c7e10b24fc8d175728994b708b7d2d774e
oral.ersoy.dokumaci <oral.ersoy.dokumaci@gmail.com>
2023-02-10 16:24:25 +0300

    Add utils.py with timer_decorator

---
+++ src/utils.py
@@ -0,0 +1,22 @@
+"""Module for utility functions."""
+
+from time import time
+from typing import Any, Callable
+
+
+def timer_decorator(func: Callable) -> Callable:
+    """Decorator that prints the time it took to execute a function."""
+
+    def wrapper_function(*args, **kwargs) -> Any:
+        """Wrapper function that prints the time it took to execute a function.
+
+        Returns:
+            Any: the result of the function
+        """
+        t1 = time()
+        result = func(*args, **kwargs)
+        t2 = time()
+        print(f"Method {func.__name__!r} took {t2 - t1:.4f} seconds to execute")
+        return result
+
+    return wrapper_function
