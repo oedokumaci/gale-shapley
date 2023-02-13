704f49db9b64600a764ac39aace3ef2a57e0e674
oral.ersoy.dokumaci <oral.ersoy.dokumaci@gmail.com>
2023-02-13 12:13:21 +0300

    CI tests readability

--- tests/algorithm_test.py
+++ tests/algorithm_test.py
@@ -15,25 +15,33 @@ def test_propose_respond_terminate(
     proposers: list[Proposer]
     responders: list[Responder]
     proposers, responders = create_deterministic_proposers_and_responders
+    m_1, m_2 = proposers
+    w_1, w_2 = responders
     algorithm = Algorithm(proposers, responders)
-    assert proposers[0].last_proposal is None
-    assert proposers[1].last_proposal is None
-    assert proposers[0].next_proposal == responders[0]
-    assert proposers[1].next_proposal == responders[0]
-    assert responders[0].current_proposals is None
-    assert responders[1].current_proposals is None
+    # Preferences of proposers and responders:
+    #    m_1 m_2 w_1 w_2
+    #    --- --- --- ---
+    # 1. w_1 w_1 m_1 m_2
+    # 2. w_2 m_2 m_2 m_1
+    # 3. m_1     w_1 w_2
+    assert m_1.last_proposal is None
+    assert m_2.last_proposal is None
+    assert m_1.next_proposal == w_1
+    assert m_2.next_proposal == w_1
+    assert w_1.current_proposals is None
+    assert w_2.current_proposals is None
     algorithm.proposers_propose()
-    assert proposers[0].last_proposal is responders[0]
-    assert proposers[1].last_proposal is responders[0]
-    assert proposers[0].next_proposal == responders[1]
-    assert proposers[1].next_proposal == proposers[1]
-    assert responders[0].current_proposals == [proposers[0], proposers[1]]
-    assert responders[1].current_proposals is None
+    assert m_1.last_proposal == w_1
+    assert m_2.last_proposal == w_1
+    assert m_1.next_proposal == w_2
+    assert m_2.next_proposal == m_2
+    assert w_1.current_proposals == [m_1, m_2]
+    assert w_2.current_proposals is None
     algorithm.responders_respond()
-    assert responders[0].current_proposals is None
-    assert responders[1].current_proposals is None
-    assert proposers[0].match == responders[0]
-    assert responders[0].match == proposers[0]
-    assert proposers[1].match is None
-    assert responders[1].match is None
+    assert w_1.current_proposals is None
+    assert w_2.current_proposals is None
+    assert m_1.match == w_1
+    assert w_1.match == m_1
+    assert m_2.match is None
+    assert w_2.match is None
     assert not algorithm.terminate()
