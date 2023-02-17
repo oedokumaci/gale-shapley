e44e41d24b81c1b45a7ec1f127b283bbcde4d721
oedokumaci <oral.ersoy.dokumaci@gmail.com>
2023-02-17 21:35:20 +0300

    Fix TwoSidedMatchingError is not used

--- src/gale_shapley/exceptions.py
+++ src/gale_shapley/exceptions.py
@@ -7,16 +7,3 @@ class GSException(Exception):
 
 class ConfigError(GSException):
     """Exception class for errors in the config.yaml file."""
-
-
-class TwoSidedMatchingError(ConfigError):
-    """Exception class for when there are not exactly two sides to be matched."""
-
-    def __init__(self, side_names: set[object]) -> None:
-        """Constructor for TwoSidedMatchingError.
-
-        Args:
-            side_names (set[Any])
-        """
-        self.message = f"Config.yaml has {len(side_names)} side(s) to be matched. There should be exactly two"
-        super().__init__(self.message)
