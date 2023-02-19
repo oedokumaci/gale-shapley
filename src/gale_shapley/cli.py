57811a52735255214d7e2d97423c0e3c4724542d
oedokumaci <oral.ersoy.dokumaci@gmail.com>
2023-02-19 10:21:10 +0300

    Fixes #1

--- src/gale_shapley/cli.py
+++ src/gale_shapley/cli.py
@@ -1,11 +1,24 @@
 """Command line application module for Gale-Shapley algorithm."""
 
+import logging
+
 from gale_shapley.config_parser import config_input
 from gale_shapley.simulator import Simulator
+from gale_shapley.utils import init_logger
 
 
 def main() -> int:
-    """Main function for Gale-Shapley algorithm."""
+    """Main function for Gale-Shapley algorithm. Logger is initialized here."""
+    init_logger(config_input.log_file_name)
+    logging.info("Parsing config.yaml is complete.")
+    logging.info(
+        f"Proposer side name: {config_input.proposer_side_name}, Responder side name: {config_input.responder_side_name}"
+    )
+    logging.info(
+        f"Number of proposers: {config_input.number_of_proposers}, Number of responders: {config_input.number_of_responders}"
+    )
+    logging.info(f"Preference type: {config_input.preference_type}")
+
     sim = Simulator(config_input)
     sim.number_of_simulations = 1000  # TODO: Make this configurable
     sim.simulate()
--- src/gale_shapley/config_parser.py
+++ src/gale_shapley/config_parser.py
@@ -1,14 +1,10 @@
-"""This module parses and validates the config.yaml.
-If the config.yaml is valid, logger is initialized here."""
+"""This module parses and validates the config.yaml."""
 
-import logging
 import os
 
 import yaml
 from pydantic import BaseModel, validator
 from pydantic.fields import ModelField
-
-from gale_shapley.utils import init_logger
 
 VALID_PREFERENCE_TYPES: tuple[str] = ("random",)
 PATH_TO_YAMLCONFIG: str = (
@@ -65,13 +61,3 @@ class YAMLConfig(BaseModel):
 
 with open(PATH_TO_YAMLCONFIG) as yaml_config:
     config_input = YAMLConfig(**yaml.safe_load(yaml_config))
-
-init_logger(config_input.log_file_name)
-logging.info("Parsing config.yaml is complete.")
-logging.info(
-    f"Proposer side name: {config_input.proposer_side_name}, Responder side name: {config_input.responder_side_name}"
-)
-logging.info(
-    f"Number of proposers: {config_input.number_of_proposers}, Number of responders: {config_input.number_of_responders}"
-)
-logging.info(f"Preference type: {config_input.preference_type}")
