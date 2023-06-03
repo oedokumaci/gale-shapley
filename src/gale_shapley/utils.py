5168a213198acfac3a7a34042bdaeb65d853c996
oedokumaci <oral.ersoy.dokumaci@gmail.com>
2023-06-03 19:31:16 +0300

    feat: add rich logging to std_out and refactor

--- src/gale_shapley/utils.py
+++ src/gale_shapley/utils.py
@@ -1,42 +1,49 @@
 """Module for utility functions."""
 
 import logging
-import sys
 from pathlib import Path
 from time import time
 from typing import Callable, TypeVar
 
+from rich.logging import RichHandler
 from typing_extensions import (
     ParamSpec,  # need typing_extensions for Python < 3.10; Paramspec is new in Python 3.10, see https://www.python.org/dev/peps/pep-0612/
 )
 
 from gale_shapley.config import YAMLConfig
 
+# Define TypeVars and ParamSpecs
 R = TypeVar("R")
 P = ParamSpec("P")
 LOG_PATH: Path = Path(__file__).parents[2] / "logs"
 
 
+# Define function to initialize the logger
 def init_logger(file_name: str) -> None:
     """Initialize the logger.
 
     Args:
-        file_name (str): the name of the log file
+        file_name (str): The name of the log file.
     """
+    # Set the log file path and create it if it does not exist
     log_file: Path = LOG_PATH / file_name
     log_file.unlink(missing_ok=True)
     log_file.touch()
+
+    # Set the log formatter and handler levels for the log file
     log_formatter = logging.Formatter("%(asctime)s:%(levelname)s: %(message)s")
     log_formatter.datefmt = "%Y-%m-%d %H:%M:%S"
-
     log_handler = logging.FileHandler(str(log_file))
     log_handler.setFormatter(log_formatter)
     log_handler.setLevel(logging.INFO)
 
-    std_log_handler = logging.StreamHandler(sys.stdout)
-    std_log_handler.setFormatter(log_formatter)
-    std_log_handler.setLevel(logging.DEBUG)
+    # Set the log formatter and handler levels for the standard output
+    std_log_formatter = logging.Formatter("%(message)s")
+    std_log_formatter.datefmt = "%H:%M:%S"
+    std_log_handler = RichHandler()
+    std_log_handler.setFormatter(std_log_formatter)
 
+    # Add handlers to the logger and set the logging level
     logger = logging.getLogger()
     logger.addHandler(std_log_handler)
     logger.addHandler(log_handler)
@@ -46,6 +53,7 @@ def init_logger(file_name: str) -> None:
     for key in logging.Logger.manager.loggerDict:
         logging.getLogger(key).setLevel(logging.ERROR)
 
+    # Log the path to log file
     logging.info(f"Path to log file: {log_file.resolve()}")
 
 
@@ -76,14 +84,24 @@ def timer_decorator(func: Callable[P, R]) -> Callable[P, R]:
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
         """Wrapper function that prints the time it took to execute a function.
 
+        Args:
+            *args (P.args): Positional arguments for the function.
+            **kwargs (P.kwargs): Keyword arguments for the function.
+
         Returns:
-            Any: the result of the function
+            R: The result of the function.
         """
+        # Get the start time and execute the function
         t1: float = time()
         result: R = func(*args, **kwargs)
+
+        # Get the end time and calculate the elapsed time
         t2: float = time()
+        elapsed_time = t2 - t1
+
+        # Log the execution time and return the result of the function
         logging.info(
-            f"Method {func.__name__!r} of module {func.__module__!r} executed in {t2 - t1:.4f} seconds."
+            f"Method {func.__name__!r} of module {func.__module__!r} executed in {elapsed_time:.4f} seconds"
         )
         return result
 
